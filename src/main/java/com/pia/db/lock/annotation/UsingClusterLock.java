cc598f104ac740de81d3f9fd67a92ee250b6ef54
abdullahbeker <abdullah.beker@pia-team.com>
2024-10-10 15:15:04 +0300

    resolve comments

--- src/main/java/com/pia/db/lock/annotation/UsingClusterLock.java
+++ src/main/java/com/pia/db/lock/annotation/UsingClusterLock.java
@@ -8,14 +8,34 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * author Abdullah Beker
+ * @author Abdullah Beker
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface UsingClusterLock {
 
-    LockType lockType();
-    boolean saveHistoryOnSuccess() default true;
-    String requestedVersion();
-    String downgradeAllowedMillis() default "0L";
+  /**
+   * Lock type to be used.
+   * */
+  LockType lockType();
+
+  /**
+   * If true, a record will be inserted into lock history table after the task is completed
+   * successfully.
+   */
+  boolean saveHistoryOnSuccess() default true;
+
+  /**
+   * The version of the lock to be retrieved.
+   *
+   * @see com.pia.db.lock.service.api.DbLockService#acquireLock(LockType, String)
+   */
+  String requestedVersion();
+
+  /**
+   * Maximum allowed duration in milliseconds to allow a downgrade.
+   *
+   * @see com.pia.db.lock.model.AcquiredLock#isDowngradeRequired(String, long)
+   */
+  String downgradeAllowedMillis() default "0L";
 }
--- src/main/java/com/pia/db/lock/annotation/impl/UsingClusterLockAnnotationAspect.java
+++ src/main/java/com/pia/db/lock/annotation/impl/UsingClusterLockAnnotationAspect.java
@@ -12,62 +12,66 @@ import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.stereotype.Component;
 
 /**
- * author Abdullah Beker
+ * @author Abdullah Beker
  */
 @Aspect
 @Component
 @RequiredArgsConstructor
 public class UsingClusterLockAnnotationAspect {
 
-    private final Environment environment;
-    private final DbLockService dbLockService;
+  private final Environment environment;
+  private final DbLockService dbLockService;
 
-    @Around("(@annotation(usingClusterLock))")
-    private Object wrapWithLock(ProceedingJoinPoint pjp, UsingClusterLock usingClusterLock) throws Throwable {
-        String requestedVersion = resolveProperty(usingClusterLock.requestedVersion());
-        long downgradeAllowedMillis = Long.parseLong(resolveProperty(usingClusterLock.downgradeAllowedMillis()));
+  @Around("(@annotation(usingClusterLock))")
+  private Object wrapWithLock(ProceedingJoinPoint pjp, UsingClusterLock usingClusterLock)
+      throws Throwable {
+    String requestedVersion = resolveProperty(usingClusterLock.requestedVersion());
+    long downgradeAllowedMillis =
+        Long.parseLong(resolveProperty(usingClusterLock.downgradeAllowedMillis()));
 
-        boolean lockReleased = false;
-        AcquiredLock lock = null;
-        try {
-            lock = dbLockService.acquireLock(usingClusterLock.lockType(), requestedVersion);
+    boolean lockReleased = false;
+    AcquiredLock lock = null;
+    try {
+      lock = dbLockService.acquireLock(usingClusterLock.lockType(), requestedVersion);
 
-            if (lock.isUpgradeRequired(requestedVersion) ||
-                    lock.isDowngradeRequired(requestedVersion, downgradeAllowedMillis)) {
+      if (lock.isUpgradeRequired(requestedVersion)
+          || lock.isDowngradeRequired(requestedVersion, downgradeAllowedMillis)) {
 
-                Object result = pjp.proceed();  // Execute the actual business logic
+        Object result = pjp.proceed(); // Execute the actual business logic
 
-                dbLockService.releaseLock(lock, usingClusterLock.saveHistoryOnSuccess());  // Release the lock after completing the task
-                lockReleased = true;
+        dbLockService.releaseLock(
+            lock,
+            usingClusterLock.saveHistoryOnSuccess()); // Release the lock after completing the task
+        lockReleased = true;
 
-                return result;
-            } else {
-                dbLockService.releaseLock(lock, false);
-                lockReleased = true;
-            }
-        } catch (Exception e) {
-            dbLockService.releaseLock(lock, false);
-            lockReleased = true;
-            throw e;
-        } finally {
-            if (!lockReleased) {
-                dbLockService.releaseLock(lock, false);
-            }
-        }
-        return null;
+        return result;
+      } else {
+        dbLockService.releaseLock(lock, false);
+        lockReleased = true;
+      }
+    } catch (Exception e) {
+      dbLockService.releaseLock(lock, false);
+      lockReleased = true;
+      throw e;
+    } finally {
+      if (!lockReleased) {
+        dbLockService.releaseLock(lock, false);
+      }
     }
+    return null;
+  }
 
-    private String resolveProperty(String value) {
-        if (value.startsWith("${") && value.endsWith("}")) {
-            return environment.getProperty(parseValue(value));
-        }
-        if (value.startsWith("#{") && value.endsWith("}")) {
-            return new SpelExpressionParser().parseExpression(parseValue(value)).getValue(String.class);
-        }
-        return value;
+  private String resolveProperty(String value) {
+    if (value.startsWith("${") && value.endsWith("}")) {
+      return environment.getProperty(parseValue(value));
     }
+    if (value.startsWith("#{") && value.endsWith("}")) {
+      return new SpelExpressionParser().parseExpression(parseValue(value)).getValue(String.class);
+    }
+    return value;
+  }
 
-    private String parseValue(String value) {
-        return value.substring(2, value.length() - 1).trim();
-    }
+  private String parseValue(String value) {
+    return value.substring(2, value.length() - 1).trim();
+  }
 }
